42f021e1d9ee9ef2445b366f0d27d0abdf7c4f10
David Rabkin <david@rabkin.co.il>
2017-07-04 12:57:39 +0300

    Added conky.

--- make.sh
+++ make.sh
@@ -12,15 +12,12 @@ dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 
 # List of files/folders to symlink in homedir.
-files="bashrc bash_profile vimrc vim zshrc oh-my-zsh tmux.conf tmux"
+files="bashrc bash_profile vimrc vim zshrc oh-my-zsh tmux.conf tmux xinitrc"
 
 # Creates dotfiles_old in homedir.
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
-echo "done."
 
 # Changes to the dotfiles directory.
-echo -n "Changing to the $dir directory ..."
 cd $dir
 echo "done."
 
@@ -28,11 +25,11 @@ echo "done."
 # then creates symlinks from the homedir to any files in the ~/dotfiles
 # directory specified in $files.
 for file in $files; do
-  echo "Moving any existing dotfiles from ~ to $olddir."
   mv ~/.$file ~/dotfiles_old/
-  echo "Creating symlink to $file in home directory."
   ln -s $dir/$file ~/.$file
 done
+
+ln -s $dir/conky ~/.config/conky
 
 # Installs needfull software.
 pkgs="zsh tmux most python cowsay htop fortune lolcat"
