1130874dcae0c83eb806ec3e022d0ed91a1ec525
David Rabkin <david@rabkin.co.il>
2018-11-18 19:25:23 +0200

    Adds stats.

--- app/rename.rb
+++ app/rename.rb
@@ -74,7 +74,7 @@ end
 class CharAction < Action
   def initialize
     # All special characters without point (.) and and (&).
-    @sym = ' (){},~\'![]_#@=“”`—’+;·‡«»$%'.chars.to_set
+    @sym = ' (){},~\'![]_#@=“”`—’+;·‡«»$%…'.chars.to_set
   end
   def do(src)
     dst = src.chars
@@ -168,10 +168,11 @@ class Renamer
   TBL_WIDTH = 79
   STR_WIDTH = (TBL_WIDTH - 9) / 2
   PTH_LIMIT = 4096
-  NME_LIMIT = 143 # Synology eCryptfs limitation.
-  #NME_LIMIT = 9 # Synology eCryptfs limitation.
+  #NME_LIMIT = 143 # Synology eCryptfs limitation.
+  NME_LIMIT = 9 # Synology eCryptfs limitation.
   def initialize
     @cfg = Configuration.new
+    @sta = { moved: 0, unaltered: 0 }
   end
   def do_dir(dir)
     raise "No such directory: #{dir}." unless File.directory?(dir)
@@ -184,17 +185,20 @@ class Renamer
       TruncateAction.new(NME_LIMIT)
     ]
     row = []
+    exi = ExistenceAction.new(dir, NME_LIMIT)
     Dir["#{dir}/*"].each { |src|
       do_dir(src) if @cfg.rec? && File.directory?(src)
       t = File.basename(src)
       act.each { |a| t = a.do(t) }
       dst = "#{dir}/#{t}"
       if (dst != src)
-        a = ExistenceAction.new(dir, NME_LIMIT)
-        t = a.do(t)
+        t = exi.do(t)
         dst = "#{dir}/#{t}"
         raise "File path exceeds #{PTH_LIMIT}: #{dst}." if dst.length > PTH_LIMIT
         FileUtils.mv(src, dst) if @cfg.act?
+        @sta[:moved] += 1
+      else
+        @sta[:unaltered] += 1
       end
       row << [
         File.basename(src)[0..STR_WIDTH],
@@ -213,7 +217,8 @@ class Renamer
   end
   def do
     do_dir(@cfg.dir)
-    puts "Done in #{@cfg.act? ? 'real' : 'simulation'} mode.".red
+    puts "#{@cfg.act? ? 'Real' : 'Simulation'}"\
+         " moved #{@sta[:moved]}, unaltered #{@sta[:unaltered]}."
   end
 end
 
