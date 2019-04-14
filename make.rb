5d87f41368bb8003cc59ef88aebeb79243d3ad85
David Rabkin <david@rabkin.co.il>
2019-04-14 19:10:27 +0300

    Removes hyper from MacOS. Adjusts X for Arch.

--- make.rb
+++ make.rb
@@ -142,7 +142,6 @@ module MacOS
     ).flatten!
      .map! { |i| DIC[i.to_sym].nil? ? i : DIC[i.to_sym] }
     # MacOS is installed without Xorg, so some graphic settings are duplicated.
-    mod.dotf << 'hyper.js'
     mod.conf << 'kitty'
     mod.test << 'brew ls --versions %s >/dev/null 2>&1'
     mod.inst << 'brew install %s || brew cask install %s'
