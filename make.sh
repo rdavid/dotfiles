8a6f9608bfaf66190a9f5ab99e9daa723347aaa0
David Rabkin <david@rabkin.co.il>
2019-08-19 17:12:58 +0300

    Adds sudo for gem install.

--- make.sh
+++ make.sh
@@ -6,44 +6,43 @@
 # This script preparies ruby environment to run make.rb.
 # Installs needfull software.
 
+platform=$(uname);
 pkgs='ruby'
-for pkg in $pkgs; do
+for p in $pkgs; do
   # Tests to see if a package is installed.
-  if command -v $pkg >/dev/null 2>&1; then
-    echo "$pkg is already installed."
+  if command -v "$p" >/dev/null 2>&1; then
+    echo "$p is already installed."
     continue
   fi
-  platform=$(uname);
   if [ "$platform" = 'Linux' ]; then
     if [ -f /etc/arch-release ]; then
-      sudo pacman --noconfirm -S $pkg
+      sudo pacman --noconfirm -S "$p"
     elif [ -f /etc/redhat-release ]; then
-      sudo yum install $pkg
+      sudo yum install "$p"
     elif [ -f /etc/debian_version ]; then
-      sudo apt-get -y install $pkg
+      sudo apt-get -y install "$p"
     fi
   elif [ "$platform" = 'Darwin' ]; then
-    brew cask install $pkg
+    brew cask install "$p"
   elif [ "$platform" = 'FreeBSD' ]; then
-    sudo pkg install $pkg devel/ruby-gems
+    sudo pkg install "$p" devel/ruby-gems
   fi
-  echo "$pkg is installed."
+  echo "$p is installed."
 done
 
 # Installs needful packages.
 gems='colorize git os'
 for g in $gems; do
   if gem list -i "$g"; then
-    echo "Gem $g is already installed."
+    echo "$g is already installed."
     continue
   fi
-  platform=$(uname);
-  if [ "$platform" = 'Darwin' ]; then
-    sudo gem install "$g"
-  else
+  if [ -f /etc/arch-release ]; then
     gem install "$g"
+  else
+    sudo gem install "$g"
   fi
-  echo "Gem $g is installed."
+  echo "$g is installed."
 done
 
 # Runs Ruby's make.
