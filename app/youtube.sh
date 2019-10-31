003a02f5aaebfc84c45ffda4ccf09d56e0253ff7
David Rabkin <david@rabkin.co.il>
2019-10-31 20:52:27 +0200

    Adds lock, log, etc.

--- app/youtube.sh
+++ app/youtube.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
+NME='youtube'
+LOG="/tmp/$NME.log"
+LCK="/tmp/$NME.lck"
+
+# Prevents multiple instances.
+if [ -e "$LCK" ] && kill -0 "$(cat "$LCK")"; then
+  echo "$0 is already running."
+  exit 0
+fi
+
+log() {
+  date +"%Y%m%d-%H:%M:%S $*" | tee -a "$LOG"
+}
+
+# Makes sure the lockfile is removed when we exit and then claim it.
+# shellcheck disable=SC2064
+trap "rm -f $LCK" INT TERM EXIT
+echo $$ > "$LCK"
+echo | tee -a "$LOG"
+log "Start $NME."
 youtube-dl \
   --playlist-reverse \
   --download-archive /media/data/app/box/youtube/done.txt \
@@ -8,4 +28,7 @@ youtube-dl \
   --merge-output-format mp4 \
   --add-metadata \
   --write-thumbnail \
-  --batch-file=/media/data/app/box/youtube/channels.txt
+  --batch-file=/media/data/app/box/youtube/channels.txt \
+  2>&1 | tee -a "$LOG"
+log "Done $NME."
+exit 0
