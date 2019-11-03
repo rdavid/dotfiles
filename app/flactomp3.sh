c0531179363c7e49e5006a9998427deccb554974
David Rabkin <david@rabkin.co.il>
2019-11-03 16:22:40 +0200

    Linted.

--- app/flactomp3.sh
+++ app/flactomp3.sh
@@ -1,15 +1,32 @@
 #!/bin/sh
-#
+# vim: tabstop=2 shiftwidth=2 expandtab textwidth=80 linebreak wrap
 # Copyright 2019-present David Rabkin
 
-FLAC_FILE="$1"
-MP3_FILE="`echo ${FLAC_FILE} | sed 's/\.flac/.mp3/'`"
+if [ 0 -eq $# ]; then
+  printf 'flactomp3.sh <file name>\n'
+  exit 0
+fi
+FLAC="$1"
+if [ ! -f "$FLAC" ]; then
+  printf 'File %s does not exist.\n' "$FLAC"
+  exit 0
+fi
+MP3=$(echo "${FLAC}" | sed 's/\.flac/.mp3/')
+printf 'Convert %s->%s, are you sure? [y/N] ' "$FLAC" "$MP3"
+CFG=$(stty -g)
+stty raw -echo; ans=$(head -c 1); stty "$CFG"
+if ! echo "$ans" | grep -iq "^y"; then
+  printf '\n'
+  exit 0
+fi
 metaflac --export-tags-to=/dev/stdout "${FLAC_FILE}" |
   sed -e 's/=/="/' -e 's/$/"/' \
     -e 's/Album=/ALBUM=/' \
     -e 's/Genre=/GENRE=/' \
     -e 's/Artist=/ARTIST=/' > /tmp/tags-$$
 cat /tmp/tags-$$
+
+# shellcheck source=/dev/null
 . /tmp/tags-$$
 rm /tmp/tags-$$
 flac -dc "${FLAC_FILE}" |
