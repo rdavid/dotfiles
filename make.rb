8742998f89f192c91ccdacb8c68834bfc6b662e6
David Rabkin <david@rabkin.co.il>
2021-09-19 21:16:12 +0300

    make: Add microsoft-remote-desktop to MacOS

--- make.rb
+++ make.rb
@@ -127,9 +127,9 @@ module MacOS
       mod.pkgs << %w[
         aerial appcleaner coreutils disk-inventory-x dropbox docker feh firefox
         hadolint ilya-birman-typography-layout iterm2 google-chrome keepassxc
-        keepingyouawake kitty launchbar librsync lolcat mpv nmap nvalt plex
-        plexamp spectacle spotify sublime-text telegram vanilla virtualbox
-        visual-studio-code watch xquartz
+        keepingyouawake kitty launchbar librsync lolcat microsoft-remote-desktop
+        mpv nmap nvalt plex plexamp spectacle spotify sublime-text telegram
+        vanilla virtualbox visual-studio-code watch xquartz
       ]
     ).flatten!
       .map! { |i| DIC[i.to_sym].nil? ? i : DIC[i.to_sym] }
