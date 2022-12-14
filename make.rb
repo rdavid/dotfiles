237737cce577177da9c8a98636ebdee1f996c238
David Rabkin <david@rabkin.co.il>
2022-12-14 19:32:04 +0200

    System monitoring with btop

--- make.rb
+++ make.rb
@@ -58,7 +58,7 @@ class OS
 
     # Packages without Xorg to install.
     @pkgs = %w[
-      atop bat boxes cairo cmake cmatrix cmus cowsay cppcheck curl ctags
+      atop bat boxes btop cairo cmake cmatrix cmus cowsay cppcheck curl ctags
       dos2unix exa f3 fdupes ffmpeg figlet fortune fzf gawk git-delta gnupg
       goredo hadolint handbrake htop imagemagick lazydocker lazygit
       librsync-dev lynx mc mosh most ncdu neofetch nnn node npm pinentry
