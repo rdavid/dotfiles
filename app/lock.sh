5439a47a8c2a1c78c7dde558e5089c6367a502ac
David Rabkin <david@rabkin.co.il>
2022-12-23 19:19:34 +0200

    Use shellbase for lock

--- app/lock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# vi:ts=2 sw=2 tw=79 et lbr wrap
-# Copyright 2021-2022 David Rabkin
-
-IMG="$HOME/dotfiles/pic/bg.jpg"
-TMP='/tmp/screen.png'
-[ -f "$TMP" ] || convert "$IMG" "$TMP"
-i3lock -t -i "$TMP"
