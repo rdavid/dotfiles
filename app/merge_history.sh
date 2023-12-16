45092c0a462c82b64a69a9834c4c9f873ca8f5a6
David Rabkin <david@rabkin.co.il>
2023-12-16 21:36:45 +0200

    Enhance comments

--- app/merge_history.sh
+++ app/merge_history.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eu
 # vi:ts=2 sw=2 tw=79 et lbr wrap
 # Copyright 2018-2023 David Rabkin
-# Magic, do not touch, see:
+# Magic, do not touch. See:
 #   https://david-kerwick.github.io/2017-01-04-combining-zsh-history-files/
 # shellcheck disable=SC2026 # This word is outside of quotes.
 cat "$@" | awk -v date="WILL_NOT_APPEAR$(date +"%s")" '{if (sub(/\\$/,date)) printf "%s", $0; else print $0}' | LC_ALL=C sort -u | awk -v date="WILL_NOT_APPEAR$(date +"%s")" '{gsub('date',"\\\n"); print $0}'
