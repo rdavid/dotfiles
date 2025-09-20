014b95eab9634cc927a3dfdb7fad08dedf702a88
David Rabkin <david@rabkin.co.il>
2025-09-20 13:45:09 +0300

    whore(python): remove depricated flag

--- install.rb
+++ install.rb
@@ -602,7 +602,7 @@ class Installer
     end
 
     # Updates all Python packages.
-    system('pip3 list --outdated --format=freeze |' \
+    system('pip3 list --outdated |' \
            'grep -v \'^\-e\' |' \
            'cut -d = -f 1 |' \
            'xargs -n1 pip3 install -U --user')
