e3fb2d2a0fb81e060b7cbdcba8645b8328df04be
David Rabkin <david@rabkin.co.il>
2026-04-17 03:24:27 +0300

    docs(comments): clarify wording in scripts

--- i3/country.rb
+++ i3/country.rb
@@ -2,22 +2,22 @@
 # frozen_string_literal: true
 
 # vi:ts=2 sw=2 tw=79 et lbr wrap
-# Copyright 2018-2022 David Rabkin
+# Copyright 2018-2026 David Rabkin
 #
-# This script prints a country name by public IP.
+# This script prints a country symbol based on the public IP.
 
 require 'English'
 require 'json'
 require 'rubygems'
 
-# Runs curl silently with 1 second time out.
+# Runs curl silently with a 1-second timeout.
 str = `curl -s -m 1 ipinfo.io`
 if !$CHILD_STATUS.success? || str.include?('timed out')
   print 'no'
   exit
 end
 
-# Extracts country name.
+# Extracts the country code.
 val = JSON.parse(str)['country'].downcase
 case val
 when 'us'
